c4f679af8edb5ef2a9307764e46dd45d83cec9c4
Matias Nicolas Mancilla <mati.nmancilla@gmail.com>
2019-12-23 11:24:37 -0300

    added core Helpers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,9 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use App\Core\ResponseHelpers;
+
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ResponseHelpers;
 }
