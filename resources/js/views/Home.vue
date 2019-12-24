c7fb9b83929d0133bbdea7bd7a60bbdee9e35050
Matias Nicolas Mancilla <mati.nmancilla@gmail.com>
2019-12-24 11:36:45 -0300

    added home view component

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Home</h1>
+        <button @click="asd()"> asdasd</button>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log("view Home")
+       
+
+    },
+    methods:{
+        asd(){
+             this.$store.commit('showLoader');
+        }
+    }
+}
+</script>
