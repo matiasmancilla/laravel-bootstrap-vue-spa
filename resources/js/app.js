4cea958c073a5613c676bbe482bd5fdd12b9f0e8
Matias Nicolas Mancilla <mati.nmancilla@gmail.com>
2019-12-24 11:34:52 -0300

    added vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,8 +30,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 import router from './router';
+import store from './common/Store';
 
 const app = new Vue({
     el: '#app',
     router,
+    store,
 });
