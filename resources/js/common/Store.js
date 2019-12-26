f78591a0eeb6ad8316cb4d672151727d4579b693
Matias Nicolas Mancilla <mati.nmancilla@gmail.com>
2019-12-26 12:46:38 -0300

    updated vuex store for loader actions

--- resources/js/common/Store.js
+++ resources/js/common/Store.js
@@ -3,14 +3,29 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+
 export default new Vuex.Store({
     state: {
-
+        // loader
+        showLoader: false,
     },
     mutations: {
 
+        // loader
+        showLoader(state) {
+            state.showLoader = true
+        },
+        hideLoader(state) {
+            state.showLoader = false
+        },
+
     },
     getters: {
+
+        // loader
+        showLoader: state => {
+            return state.showLoader
+        },
 
     }
 });
