aa74670906dbaaa61000da398e4bb8d218ab2d21
Matias Nicolas Mancilla <mati.nmancilla@gmail.com>
2019-12-26 12:49:16 -0300

    added new view Forms

---
+++ resources/js/views/Forms.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>Forms</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log("view Forms")
+    },
+    methods:{
+       
+    }
+}
+</script>
